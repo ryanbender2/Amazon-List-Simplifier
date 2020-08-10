4aadb00ddae395be73ab78a8590fe72d755a852a
Ryan Bender <ryanbender65@yahoo.com>
2020-08-10 19:36:00 -0400

    Update main.py

--- server-side/main.py
+++ server-side/main.py
@@ -1,1 +1,12 @@
-print("test")
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+
+class ALS_Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Hello", "World")
+        self.end_headers()
+
+server_address = ("localhost", 5406)
+httpd = HTTPServer(server_address, ALS_Server)
+httpd.serve_forever()
